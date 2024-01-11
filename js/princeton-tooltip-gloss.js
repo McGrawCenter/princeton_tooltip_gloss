ae1d03fe7c0ebd5ba8e1a1de5da1411e823e8394
benjohnsto <benj@princeton.edu>
2024-01-11 06:13:06 -0500

    js change

--- js/princeton-tooltip-gloss.js
+++ js/princeton-tooltip-gloss.js
@@ -1,16 +1,24 @@
+jQuery(document).ready(function(){
 
 
-jQuery(".glossary").hover(function(e){
-    e.preventDefault();
-    var position = jQuery(this).position();
-    var popup = jQuery(this).next(".popup");
-    var popup_height = popup.height();
-    popup.css({top: position.top-(popup_height + 20), left: position.left, position:'absolute'});
-    popup.fadeToggle("fast");
-    popup.css({ display: 'table'});
+
+	jQuery(".glossary").hover(function(e){
+	    e.preventDefault();
+	    var position = jQuery(this).position();
+	    var popup = jQuery(this).next(".popup");
+	    var popup_height = popup.height();
+	    popup.css({top: position.top-(popup_height + 20), left: position.left, position:'absolute'});
+	    popup.fadeToggle("fast");
+	    popup.css({ display: 'table'});
+
+	});
+
+	jQuery(".glossary").click(function(e){
+	    e.preventDefault();
+	});
+
+
 
 });
 
-jQuery(".glossary").click(function(e){
-    e.preventDefault();
-});
+
